093e960596c8e1385813874a17d06ba7fe9b44da
Fatih <fatihcihan666@gmail.com>
2024-03-08 21:14:57 +0300

    Home, Create, Details and Search components imported

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,7 @@
+import Home from "./pages/home/Home";
+import Create from "./pages/create/Create";
+import Details from "./pages/details/Details";
+import Search from "./pages/search/Search";
 
 function App() {
   return <div>App</div>;
