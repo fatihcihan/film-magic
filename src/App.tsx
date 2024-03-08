579abb32966a692160f8d94d97981d92fc2ddf68
Fatih <fatihcihan666@gmail.com>
2024-03-08 21:53:54 +0300

    updated App.tsx to implement routing using react-router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,18 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Home from "./pages/home/Home";
 import Create from "./pages/create/Create";
 import Details from "./pages/details/Details";
 import Search from "./pages/search/Search";
 
+const routes = createBrowserRouter([
+  { path: "/", element: <Home /> },
+  { path: "/create", element: <Create /> },
+  { path: "/details/:id", element: <Details /> },
+  { path: "/search", element: <Search /> },
+]);
+
 function App() {
-  return <div>App</div>;
+  return <RouterProvider router={routes} />;
 }
 
 export default App;
