5a6427e6c4e3ac694167279516db04df7b26db05
Fatih <fatihcihan666@gmail.com>
2024-03-11 23:31:47 +0300

    refactored the Home component to dynamically pull and show movie data

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,20 +1,62 @@
-import Card from 'react-bootstrap/Card';
-import Button from 'react-bootstrap/Button';
+import Card from "react-bootstrap/Card";
+import Button from "react-bootstrap/Button";
+import axios, { AxiosResponse } from "axios";
+import { useEffect, useState } from "react";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 import "./Home.css";
 
+interface Movie {
+  id: string;
+  title: string;
+  overview: string;
+}
+
 function Home() {
+  const [movies, setMovies] = useState<Movie[] | null>([]);
+
+  useEffect(() => {
+    const fetchMovies = async () => {
+      try {
+        const response: AxiosResponse<any> = await axios.get(
+          "https://api.themoviedb.org/3/movie/popular?language=en-US&page=1",
+          {
+            headers: {
+              Accept: "application/json",
+              Authorization:
+                "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJhYjE2YjdiY2Y5ODlhOTU4YmUxZTZlOWI2MTU4MDRhYiIsInN1YiI6IjY0MjRiNjZiYzk5ODI2MDBiNjAwMWNlYyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.dqNPCxMi4kDfz7NQVV1287oeLEJqlKoAWHDyksGEUfU",
+            },
+          }
+        );
+        const data: Movie[] = response.data.results;
+        console.log(data, "data");
+        setMovies(data);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+    fetchMovies();
+  }, []);
+
   return (
-    <Card style={{ width: "18rem" }}>
-      <Card.Img variant="top" src="" />
-      <Card.Body>
-        <Card.Title>Card Title</Card.Title>
-        <Card.Text>
-          Some quick example text to build on the card title and make up the
-          bulk of the card's content.
-        </Card.Text>
-        <Button variant="primary">Go somewhere</Button>
-      </Card.Body>
-    </Card>
+    <Container>
+      <Row>
+        {movies &&
+          movies.map((movie) => (
+            <Col key={movie.id}>
+              <Card style={{ width: "18rem" }}>
+                <Card.Img variant="top" src="" />
+                <Card.Body>
+                  <Card.Title>{movie.title}</Card.Title>
+                  <Card.Text>{movie.overview}</Card.Text>
+                  <Button variant="primary">Go somewhere</Button>
+                </Card.Body>
+              </Card>
+            </Col>
+          ))}
+      </Row>
+    </Container>
   );
 }
 
