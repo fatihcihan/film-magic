ff039a5a91609f098d7cffcb19c34f450a3ebcdc
Fatih <fatihcihan666@gmail.com>
2024-03-11 14:57:57 +0300

    refactored the routing structure to use MainLayout as the main route

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,22 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import 'bootstrap/dist/css/bootstrap.min.css';
 import Home from "./pages/home/Home";
 import Create from "./pages/create/Create";
 import Details from "./pages/details/Details";
 import Search from "./pages/search/Search";
+import MainLayout from "./layout/MainLayout";
 
 const routes = createBrowserRouter([
-  { path: "/", element: <Home /> },
-  { path: "/create", element: <Create /> },
-  { path: "/details/:id", element: <Details /> },
-  { path: "/search", element: <Search /> },
+  {
+    path: "/",
+    element: <MainLayout />,
+    children: [
+      { path: "/", element: <Home /> },
+      { path: "/create", element: <Create /> },
+      { path: "/details/:id", element: <Details /> },
+      { path: "/search", element: <Search /> },
+    ],
+  },
 ]);
 
 function App() {
