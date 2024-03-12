49d930e5b050ba9dcdde00f31cd7943ecb85bfda
Fatih <fatihcihan666@gmail.com>
2024-03-12 23:42:38 +0300

    Col component removed, unnecessary fields cleaned up

--- src/components/MovieCard.tsx
+++ src/components/MovieCard.tsx
@@ -1,28 +1,25 @@
 import React from "react";
-import { Button, Card, Col } from "react-bootstrap";
+import { Button, Card } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import { Movie } from "../types/Movie";
 
 function MovieCard(props: { movie: Movie }) {
   const imageUrl = process.env.REACT_APP_IMAGE_URL;
   const { movie } = props;
-  console.log(movie);
 
   return (
-    <Col key={movie.id}>
-      <Card style={{ width: "18rem" }}>
-        <Card.Img variant="top" src={`${imageUrl}/${movie.poster_path}`} />
-        <Card.Body>
-          <Card.Title>{movie.title}</Card.Title>
-          <Card.Text>{movie.overview}</Card.Text>
-          <Link to="test">
-            <Button variant="outline-primary" size="lg">
-              Details
-            </Button>
-          </Link>
-        </Card.Body>
-      </Card>
-    </Col>
+    <Card style={{ width: "18rem" }}>
+      <Card.Img variant="top" src={`${imageUrl}/${movie.poster_path}`} />
+      <Card.Body>
+        <Card.Title>{movie.title}</Card.Title>
+        <Card.Text>{movie.overview}</Card.Text>
+        <Link to="test">
+          <Button variant="outline-primary" size="lg">
+            Details
+          </Button>
+        </Link>
+      </Card.Body>
+    </Card>
   );
 }
 
