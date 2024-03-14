cc7a08f9b3edb7d54a507aa528a7b94d8741655f
Fatih <fatihcihan666@gmail.com>
2024-03-14 12:13:50 +0300

    created movie summary shortening function in MovieCard component

--- src/components/MovieCard.tsx
+++ src/components/MovieCard.tsx
@@ -7,19 +7,29 @@ function MovieCard(props: { movie: Movie }) {
   const imageUrl = process.env.REACT_APP_IMAGE_URL;
   const { movie } = props;
 
+  const shortenOverview = (overview: string, maxLength: number) => {
+    if (overview.length <= maxLength) {
+      return overview;
+    } else {
+      return overview.slice(0, maxLength - 3) + "...";
+    }
+  };
+
   return (
-    <Card style={{ width: "18rem", margin:'0 18px' }} >
-      <Card.Img variant="top" src={`${imageUrl}/${movie.poster_path}`} />
-      <Card.Body>
-        <Card.Title>{movie.title}</Card.Title>
-        <Card.Text>{movie.overview}</Card.Text>
-        <Link to={`/details/${movie.id}`}>
-          <Button variant="outline-primary" size="lg">
-            Details
-          </Button>
-        </Link>
-      </Card.Body>
-    </Card>
+    <>
+      <Card style={{ width: "18rem", margin: "0 18px", marginBottom: "30px" }}>
+        <Card.Img variant="top" src={`${imageUrl}/${movie.poster_path}`} />
+        <Card.Body>
+          <Card.Title>{movie.title}</Card.Title>
+          <Card.Text>{shortenOverview(movie.overview, 100)}</Card.Text>
+          <Link to={`/details/${movie.id}`}>
+            <Button variant="outline-primary" size="lg">
+              Details
+            </Button>
+          </Link>
+        </Card.Body>
+      </Card>
+    </>
   );
 }
 
