e4e77ee439a07ab95d0f4361755c83186085eccb
Fatih <fatihcihan666@gmail.com>
2024-03-15 22:21:48 +0300

    edited with useState hook for state management and added functionality for search input changes

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -1,16 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
 import { Container, Form, InputGroup } from "react-bootstrap";
 
-function Search() {
+const Search: React.FC<{ onSearch: (searchValue: string) => void }> = ({
+  onSearch,
+}): JSX.Element => {
+  const [searchInput, setSearchInput] = useState<string>("");
+
+  const handleSearch = (event: any) => {
+    const { value } = event.target;
+    console.log(value);
+    setSearchInput(value);
+    onSearch(value);
+  };
+
+  const handleSubmit = (event: any) => {
+    event.preventDefault();
+  };
+
   return (
     <Container>
-    <InputGroup style={{ marginTop: '1rem', marginBottom: '1rem' }}>
-      <Form.Control
-        placeholder="Search Movie"
-      />
-    </InputGroup>
+      <Form onSubmit={handleSubmit}>
+        <InputGroup style={{ marginTop: "1rem", marginBottom: "1rem" }}>
+          <Form.Control
+            type="text"
+            placeholder="Search Movie"
+            value={searchInput}
+            onChange={handleSearch}
+          />
+        </InputGroup>
+      </Form>
     </Container>
   );
-}
+};
 
 export default Search;
