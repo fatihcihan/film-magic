a1eab18586b2060cd11b705d24f4d82e59330391
Fatih <fatihcihan666@gmail.com>
2024-03-19 16:51:32 +0300

    integrated API call to fetch top-rated movies in TopRated page

--- src/pages/top-rated/TopRated.tsx
+++ src/pages/top-rated/TopRated.tsx
@@ -1,8 +1,44 @@
-import React from "react";
-import { Container } from "react-bootstrap";
+import React, { useEffect, useState } from "react";
+import { Container, Row } from "react-bootstrap";
+import { Movie } from "../../types/Movie";
+import axios, { AxiosResponse } from "axios";
 
 const TopRated: React.FC = (): JSX.Element => {
-  return <Container>Top Rated Page</Container>;
+  const [movies, setMovies] = useState<Movie[] | null>([]);
+  const accessToken = process.env.REACT_APP_API_ACCESS_TOKEN;
+
+  const fetchMovies = async () => {
+    try {
+      const response: AxiosResponse<any> = await axios.get(
+        "https://api.themoviedb.org/3/movie/top_rated?language=en-US&page=1",
+        {
+          headers: {
+            Accept: "application/json",
+            Authorization: `Bearer ${accessToken}`,
+          },
+        }
+      );
+      const data = response.data.results;
+      console.log(data, "top rated movie");
+      setMovies(data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchMovies();
+  }, []);
+
+  return (
+    <Container>
+      <Row lg={4}>
+        {movies?.map((movie) => (
+          <div>{movie.title}</div>
+        ))}
+      </Row>
+    </Container>
+  );
 };
 
 export default TopRated;
