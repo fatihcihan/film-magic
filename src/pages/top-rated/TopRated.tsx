00d7c179934feb44c1b6194719ae08fdf613216b
Fatih <fatihcihan666@gmail.com>
2024-03-19 14:15:55 +0300

    added TopRated page under pages directory

---
+++ src/pages/top-rated/TopRated.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { Container } from "react-bootstrap";
+
+const TopRated: React.FC = (): JSX.Element => {
+  return <Container>Top Rated Page</Container>;
+};
+
+export default TopRated;
