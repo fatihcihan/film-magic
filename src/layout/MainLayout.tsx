f06e48eee6a69ea7cef1abe531f0b49f393efa40
Fatih <fatihcihan666@gmail.com>
2024-03-25 22:53:44 +0300

    added HeroSection component under Header component

--- src/layout/MainLayout.tsx
+++ src/layout/MainLayout.tsx
@@ -2,15 +2,17 @@ import Container from "react-bootstrap/Container";
 import { Outlet } from "react-router-dom";
 import Footer from "../components/Footer/Footer";
 import Header from "../components/Header/Header";
+import HeroSection from "../components/HeroSection/HeroSection";
 
 function MainLayout() {
   return (
     <>
       <Header />
+      <HeroSection />
       <Container>
         <Outlet />
       </Container>
-      <Footer/>
+      <Footer />
     </>
   );
 }
