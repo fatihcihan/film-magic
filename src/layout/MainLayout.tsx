c209fdc11e07af44c7594c922041c8940eaeea08
Fatih <fatihcihan666@gmail.com>
2024-03-26 23:47:06 +0300

    refactored MainLayout to conditionally render HeroSection based on current URL

--- src/layout/MainLayout.tsx
+++ src/layout/MainLayout.tsx
@@ -1,14 +1,16 @@
 import Container from "react-bootstrap/Container";
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation } from "react-router-dom";
 import Footer from "../components/Footer/Footer";
 import Header from "../components/Header/Header";
 import HeroSection from "../components/HeroSection/HeroSection";
 
 function MainLayout() {
+  const location = useLocation();
+  const showHeroSection = location.pathname === "/";
   return (
     <>
       <Header />
-      <HeroSection />
+      {showHeroSection && <HeroSection />}
       <Container>
         <Outlet />
       </Container>
