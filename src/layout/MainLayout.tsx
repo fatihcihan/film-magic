deacc865e7e3de7aa7170bafe3b9e6b35611679e
Fatih <fatihcihan666@gmail.com>
2024-03-29 16:45:06 +0300

    MainLayout component updated, added AboutUsSection and ProjectSection based on route location

--- src/layout/MainLayout.tsx
+++ src/layout/MainLayout.tsx
@@ -3,14 +3,20 @@ import { Outlet, useLocation } from "react-router-dom";
 import Footer from "../components/Footer/Footer";
 import Header from "../components/Header/Header";
 import HeroSection from "../components/HeroSection/HeroSection";
+import ProjectSection from "../components/ProjectSection/ProjectSection";
+import AboutUsSection from "../components/AboutUsSection/AboutUsSection";
 
 function MainLayout() {
   const location = useLocation();
   const showHeroSection = location.pathname === "/";
+  const projetSection = location.pathname === "/";
+  const aboutUsSection = location.pathname === "/";
   return (
     <>
       <Header />
       {showHeroSection && <HeroSection />}
+      {aboutUsSection && <AboutUsSection />}
+      {projetSection && <ProjectSection />}
       <Container>
         <Outlet />
       </Container>
