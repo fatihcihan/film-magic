c8259bcfee88df2f0aeaf3fb8a6d541efafb1933
Fatih <fatihcihan666@gmail.com>
2024-03-29 23:26:39 +0300

    added Divider section component

---
+++ src/components/DividerSection/DividerSection.tsx
@@ -0,0 +1,61 @@
+import React from "react";
+import { Button, Col, Container, Form, InputGroup, Row } from "react-bootstrap";
+
+const DividerSection = () => {
+  return (
+    <div>
+      <section id="divider" className="generic dark-bg">
+        <Container>
+          <Row>
+            <Col lg={8}>
+              <Form>
+                <InputGroup className="mb-3">
+                  <Form.Control
+                    className="form-control rounded-0 border-danger text-white bg-transparent"
+                    size="lg"
+                    type="email"
+                    placeholder="@JOIN NEWSLETTER"
+                  />
+                  <Button
+                    className="btn btn-danger rounded-0 text-white border-danger"
+                    size="lg"
+                    type="submit"
+                  >
+                    SUBSCRIBE
+                  </Button>
+                </InputGroup>
+              </Form>
+            </Col>
+          </Row>
+        </Container>
+      </section>
+    </div>
+  );
+};
+
+export default DividerSection;
+
+/* 
+{ <Form>
+    <Form.Control
+      className="form-control rounded-0 border-danger text-white bg-transparent"
+      size="lg"
+      type="email"
+      placeholder="@JOIN NEWSLETTER"
+    />
+    <InputGroup size="lg">
+     
+      <Button
+        className="btn btn-danger rounded-0 text-white border-danger"
+        as="input"
+        type="submit"
+        value="SUBSCRIBE"
+      />
+    </InputGroup>
+  </Form>} */
+
+{
+  /* <InputGroup.Text id="inputGroup-sizing-lg">
+        Large
+      </InputGroup.Text> */
+}
