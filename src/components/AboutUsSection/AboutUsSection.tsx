a82672a78383354fe22c653a4a8505a62bf6f288
Fatih <fatihcihan666@gmail.com>
2024-04-01 22:41:12 +0300

    edited Col component for responsive design

--- src/components/AboutUsSection/AboutUsSection.tsx
+++ src/components/AboutUsSection/AboutUsSection.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Col, Container, Row } from "react-bootstrap";
-import './AboutUsSection.css'
+import "./AboutUsSection.css";
 
 const AboutUsSection = () => {
   return (
@@ -8,7 +8,7 @@ const AboutUsSection = () => {
       <Container>
         <h2 className="display-5 text-danger mb-4">WHO WE ARE?</h2>
         <Row className="mb-3">
-          <Col className="col-8 bg-light p-0">
+          <Col sm="12" md="8" className="bg-light p-0">
             <div className="d-flex h-100 flex-column justify-content-center">
               <p>
                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum
@@ -21,7 +21,7 @@ const AboutUsSection = () => {
               </p>
             </div>
           </Col>
-          <Col className="col-4 p-0">
+          <Col sm="12" md="4" className="p-0">
             <img
               className="img-fluid"
               src="https://images.unsplash.com/photo-1515634928627-2a4e0dae3ddf?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTZ8fG1vdmllJTIwY29tcGFueXxlbnwwfHwwfHx8MA%3D%3D"
@@ -30,7 +30,7 @@ const AboutUsSection = () => {
         </Row>
 
         <Row>
-          <Col className="col-8 bg-light">
+          <Col sm="12" md="8" className="bg-light">
             <div className="d-flex h-100 flex-column justify-content-center">
               <p>
                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum
@@ -43,7 +43,7 @@ const AboutUsSection = () => {
               </p>
             </div>
           </Col>
-          <Col className="col-4 order-first p-0">
+          <Col sm="12" md="4" className="order-first p-0">
             <img
               className="img-fluid"
               src="https://images.unsplash.com/photo-1612544409025-e1f6a56c1152?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8bW92aWUlMjBjb21wYW55fGVufDB8fDB8fHww"
