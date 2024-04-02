62e4e4def7fa9be60cfca462eb5ee7a6ee532a48
Fatih <fatihcihan666@gmail.com>
2024-04-02 23:44:02 +0300

    related design has been made responsive

--- src/components/BlogSection/BlogSection.tsx
+++ src/components/BlogSection/BlogSection.tsx
@@ -9,7 +9,7 @@ const BlogSection = () => {
       <Container>
         <h2 className="display-5 text-danger mb-5">WHAT WE SAY?</h2>
         <Row>
-          <Col>
+          <Col md="6" lg="4">
             <Card className="bg-light border-0">
               <Card.Img
                 variant="top"
@@ -42,9 +42,9 @@ const BlogSection = () => {
               </Card.Body>
             </Card>
           </Col>
-          <Col>
+          <Col md="6" lg="4">
             <Card className="bg-light border-0">
-              <Card.Img 
+              <Card.Img
                 variant="top"
                 src="https://images.unsplash.com/photo-1585951237313-1979e4df7385?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MzF8fG1vdmllfGVufDB8fDB8fHww"
               ></Card.Img>
@@ -75,7 +75,40 @@ const BlogSection = () => {
               </Card.Body>
             </Card>
           </Col>
-          <Col>
+          <Col md="6" lg="4">
+            <Card className="bg-light border-0">
+              <Card.Img
+                variant="top"
+                src="https://images.unsplash.com/photo-1496065187959-7f07b8353c55?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTJ8fHRlY2hub2xvZ3l8ZW58MHx8MHx8fDA%3D"
+              ></Card.Img>
+              <Card.Body>
+                <h5 className="lead text-uppercase text-danger mb-4">
+                  5 OUTRAGEOUS IDEAS FOR YOUR TECHNOLOGCY
+                </h5>
+                <h6 className="card-title text-danger text-uppercase">
+                  ARMANDA DIEGO
+                </h6>
+                <Card.Text>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi
+                  accusamus ipsa fugiat eius provident ducimus consectetur
+                  adipisci et doloribus. Maiores?
+                </Card.Text>
+                <ul className="list-inline">
+                  <li className="list-inline-item">
+                    <small className="text-muted">
+                      <LuClock /> 2 Days Ago |
+                    </small>
+                  </li>
+                  <li className="list-inline-item">
+                    <small className="text-muted">
+                      <MdOutlineInsertComment /> 10
+                    </small>
+                  </li>
+                </ul>
+              </Card.Body>
+            </Card>
+          </Col>
+          <Col md="6" lg="4" className="d-none d-md-block d-lg-none">
             <Card className="bg-light border-0">
               <Card.Img
                 variant="top"
