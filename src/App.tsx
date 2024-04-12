8051ea05c9c0843c967ff168a701ca2c4636537f
Fatih <fatihcihan666@gmail.com>
2024-04-12 17:18:29 +0300

    router operations and layout refactored

--- src/App.tsx
+++ src/App.tsx
@@ -8,21 +8,73 @@ import PopularMovies from "./pages/movies/popular/PopularMovies";
 import PopularSeries from "./pages/tv-series/popular/PopularSeries";
 import TopRatedSeries from "./pages/tv-series/top-rated/TopRatedSeries";
 import { MyMovieList } from "./components/MyMovieList/MyMovieList";
+import HeroSection from "./components/HeroSection/HeroSection";
+import AboutUsSection from "./components/AboutUsSection/AboutUsSection";
+import ProjectSection from "./components/ProjectSection/ProjectSection";
+import BlogSection from "./components/BlogSection/BlogSection";
+import DividerSection from "./components/DividerSection/DividerSection";
+import ApproachSection from "./components/ApproachSection/ApproachSection";
+import TeamSection from "./components/TeamSection/TeamSection";
+import ContactSection from "./components/ContactSection/ContactSection";
 
 const routes = createBrowserRouter([
   {
     path: "/",
     element: <MainLayout />,
     children: [
-      // { index: true, element: <Home /> },
-      // { path: "/", element: <Home /> },
-      { path: "/details/:id", element: <Details /> },
-      { path: "/movies/popular", element: <PopularMovies /> },
-      { path: "/movies/top-rated", element: <TopRatedMovies /> },
-      { path: "/tv-series/popular", element: <PopularSeries /> },
-      { path: "/tv-series/top-rated", element: <TopRatedSeries /> },
-      { path: "/my-list/movie", element: <MyMovieList /> },
+      {
+        path: "",
+        element: (
+          <>
+            <HeroSection />
+            <ProjectSection />
+            <AboutUsSection />
+            <BlogSection />
+            <DividerSection />
+            <ApproachSection />
+            <TeamSection />
+            <ContactSection />
+          </>
+        ),
+      },
+      {
+        path: "home",
+        element: (
+          <>
+            <HeroSection />
+            <ProjectSection />
+            <AboutUsSection />
+            <BlogSection />
+            <DividerSection />
+            <ApproachSection />
+            <TeamSection />
+            <ContactSection />
+          </>
+        ),
+      },
     ],
+  },
+  {
+    path: "/movies",
+    element: <MainLayout />,
+    children: [
+      { path: "popular", element: <PopularMovies /> },
+      { path: "top-rated", element: <TopRatedMovies /> },
+      { path: "popular/details/:id", element: <Details /> },
+    ],
+  },
+  {
+    path: "/tv-series",
+    element: <MainLayout />,
+    children: [
+      { path: "popular", element: <PopularSeries /> },
+      { path: "top-rated", element: <TopRatedSeries /> },
+    ],
+  },
+  {
+    path: "/my-list",
+    element: <MainLayout />,
+    children: [{ path: "movie", element: <MyMovieList /> }],
   },
 ]);
 
