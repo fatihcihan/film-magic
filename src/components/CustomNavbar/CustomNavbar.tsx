8df9716bba8f46fd0d917c9cab7f5731c80c7a4f
Fatih <fatihcihan666@gmail.com>
2024-04-12 17:28:10 +0300

    added TypeScript typing to CustomNavbar component

--- src/components/CustomNavbar/CustomNavbar.tsx
+++ src/components/CustomNavbar/CustomNavbar.tsx
@@ -5,7 +5,7 @@ import Navbar from "react-bootstrap/Navbar";
 import { Link } from "react-router-dom";
 import { NavDropdown } from "react-bootstrap";
 
-const CustomNavbar: React.FC = () => {
+const CustomNavbar: React.FC = (): JSX.Element => {
   return (
     <>
       <Navbar
@@ -16,7 +16,7 @@ const CustomNavbar: React.FC = () => {
         className="py-3"
       >
         <Container>
-          <Navbar.Brand as={Link} to="/">
+          <Navbar.Brand as={Link} to="/home">
             Film Magic
           </Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
