2c5fa23469cb19e297c06461b6d4450b586edf34
Fatih <fatihcihan666@gmail.com>
2024-04-12 17:28:34 +0300

    added TypeScript typing to Footer component

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,6 +1,6 @@
 import { Container } from "react-bootstrap";
 
-function Footer() {
+const Footer: React.FC = (): JSX.Element => {
   const currentYear = new Date().getFullYear();
   return (
     <footer>
@@ -9,6 +9,6 @@ function Footer() {
       </Container>
     </footer>
   );
-}
+};
 
 export default Footer;
