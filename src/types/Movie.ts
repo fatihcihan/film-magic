092d82161acc4d1a740a95035dee0970e10cb406
Fatih <fatihcihan666@gmail.com>
2024-04-23 14:58:56 +0300

    added additional properties and genres array with MovieGenre type

--- src/types/Movie.ts
+++ src/types/Movie.ts
@@ -1,3 +1,5 @@
+import { MovieGenre } from "./MovieGenre";
+
 export class Movie {
     id!: string;
     title!: string;
@@ -6,4 +8,11 @@ export class Movie {
     backdrop_path!: string;
     vote_average!: number;
     homepage!: string;
+    release_date!: string;
+    tagline!: string;
+    vote_count!: number;
+    budget!: number;
+    runtime!: number;
+    revenue!: number;
+    genres!: MovieGenre[];
 }
