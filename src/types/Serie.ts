0cf0eeae58b2facbc221760ea1249ee7466de6e7
Fatih <fatihcihan666@gmail.com>
2024-04-24 18:29:44 +0300

    necessary properties added

--- src/types/Serie.ts
+++ src/types/Serie.ts
@@ -1,7 +1,15 @@
+import { MovieGenre } from "./MovieGenre";
+
 export class Serie {
     id!: string;
     name!: string;
+    title!: string;
+    tagline!: string;
+    first_air_date!: string;
+    last_air_date!: string;
     overview!: string;
     vote_average!: number;
     poster_path!: string;
+    number_of_seasons!:number;
+    genres!: MovieGenre[];
 }
