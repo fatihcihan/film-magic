1b65da049bd5f6fd2b618be91af36492d4d8547b
Fatih <fatihcihan666@gmail.com>
2024-08-01 11:08:38 +0300

    fetchMoviesByGenre function written, movies state defined, unnecessary fields cleaned

--- src/components/ProjectSection/ProjectSection.tsx
+++ src/components/ProjectSection/ProjectSection.tsx
@@ -8,10 +8,11 @@ import { MovieGenre } from "../../types/MovieGenre";
 import { Movie } from "../../types/Movie";
 
 const ProjectSection = () => {
-
   const [openGenre, setOpenGenre] = useState<number | null>(null);
   const [genres, setGenres] = useState<MovieGenre[] | null>([]);
+  const [movies, setMovies] = useState<Movie[] | null>(null);
   const accessToken = process.env.REACT_APP_API_ACCESS_TOKEN;
+  const imageUrl = process.env.REACT_APP_IMAGE_URL;
 
   const imageUrls = [
     "https://images.unsplash.com/photo-1618945524163-32451704cbb8?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
@@ -33,11 +34,37 @@ const ProjectSection = () => {
       );
       const genres: MovieGenre[] = response.data.genres;
       setGenres(genres);
-      console.log(genres, "genres");
     } catch (err) {
       console.log("error");
     }
   };
+
+  const fetchMoviesByGenre = async (genreId: number) => {
+    try {
+      console.log(genreId);
+      const response: AxiosResponse<any> = await axios.get(
+        `https://api.themoviedb.org/3/discover/movie?with_genres=${genreId}&language=en`,
+        {
+          headers: {
+            Accept: "application/json",
+            Authorization: `Bearer ${accessToken}`,
+          },
+        }
+      );
+      const randomizedMovies = response.data.results.sort(
+        () => 0.5 - Math.random()
+      );
+      setMovies(randomizedMovies.slice(0, 3));
+    } catch (err) {
+      console.log("error fetching movies", err);
+    }
+  };
+
+  useEffect(() => {
+    if (openGenre !== null) {
+      fetchMoviesByGenre(openGenre);
+    }
+  }, [openGenre]);
 
   useEffect(() => {
     fetchGenres();
@@ -69,82 +96,6 @@ const ProjectSection = () => {
               </div>
             </Col>
           ))}
-
-          {/*  <Col xl="3" sm="6" className="p-0">
-            <div className="latest-item">
-              <Button
-                onClick={() => setOpen1(!open1)}
-                aria-controls="example-collapse-text"
-                aria-expanded={open1}
-                className="p-0 rounded-0 border-0"
-              >
-                <Image
-                  src="https://images.unsplash.com/photo-1548438294-1ad5d5f4f063?q=80&w=1472&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-                  fluid
-                />
-                <div className="overlay d-flex align-items-center justify-content-center">
-                  <HiChevronDoubleDown className="chevron-icon" />
-                </div>
-              </Button>
-            </div>
-          </Col>
-
-          <Col xl="3" sm="6" className="p-0">
-            <div className="latest-item">
-              <Button
-                onClick={() => setOpen2(!open2)}
-                aria-controls="example-collapse-text"
-                aria-expanded={open2}
-                className="p-0 rounded-0 border-0"
-              >
-                <Image
-                  src="https://images.unsplash.com/photo-1537884944318-390069bb8665?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTB8fGNvZGV8ZW58MHx8MHx8fDA%3D"
-                  fluid
-                />
-                <div className="overlay d-flex align-items-center justify-content-center">
-                  <HiChevronDoubleDown className="chevron-icon" />
-                </div>
-              </Button>
-            </div>
-          </Col>
-
-          <Col xl="3" sm="6" className="p-0">
-            <div className="latest-item">
-              <Button
-                onClick={() => setOpen3(!open3)}
-                aria-controls="example-collapse-text"
-                aria-expanded={open3}
-                className="p-0 rounded-0 border-0"
-              >
-                <Image
-                  src="https://images.unsplash.com/photo-1594377157609-5c996118ac7f?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-                  fluid
-                />
-                <div className="overlay d-flex align-items-center justify-content-center">
-                  <HiChevronDoubleDown className="chevron-icon" />
-                </div>
-              </Button>
-            </div>
-          </Col>
-
-          <Col xl="3" sm="6" className="p-0">
-            <div className="latest-item">
-              <Button
-                onClick={() => setOpen4(!open4)}
-                aria-controls="example-collapse-text"
-                aria-expanded={open4}
-                className="p-0 rounded-0 border-0"
-              >
-                <Image
-                  src="https://images.unsplash.com/photo-1503551723145-6c040742065b-v2?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-                  fluid
-                />
-                <div className="overlay d-flex align-items-center justify-content-center">
-                  <HiChevronDoubleDown className="chevron-icon" />
-                </div>
-              </Button>
-            </div>
-          </Col> */}
         </Row>
 
         <Row>
@@ -153,175 +104,45 @@ const ProjectSection = () => {
               <Container fluid className="p-0 mt-5">
                 <Row className="p-0">
                   <Col sm="6" xl="3">
-                    <CloseButton onClick={() => setOpenGenre(null)} />
+                    <CloseButton
+                      onClick={() => {
+                        setOpenGenre(null);
+                      }}
+                    />
                     <dl className="row ps-5">
                       <dt className="col-12">
                         <span className="text-danger lead">{genre.name}</span>
-                      </dt>                     
-                      {/* Ek bilgi yerleştirilebilir */}
+                      </dt>
+                      <dd className="col-12">
+                        {movies ? (
+                          <ul>
+                            {movies.map((movie) => (
+                              <li key={movie.id}>{movie.title}</li>
+                            ))}
+                          </ul>
+                        ) : (
+                          <p>Loading...</p>
+                        )}
+                      </dd>
                     </dl>
                   </Col>
-                  {/* Ek görseller ya da bilgiler yerleştirilebilir */}
+                  {movies ? (
+                    movies.map((movie) => (
+                      <Col sm="6" xl="3" className="p-0">
+                        <Image
+                          key={movie.id}
+                          src={`${imageUrl}/${movie.backdrop_path}`}
+                          fluid
+                        />
+                      </Col>
+                    ))
+                  ) : (
+                    <p>Loading...</p>
+                  )}
                 </Row>
               </Container>
             </Collapse>
           ))}
-
-          {/*  <Collapse in={open1}>
-            <Container fluid className="p-0 mt-5">
-              <Row className="p-0" shadow="true">
-                <Col sm="6" xl="3">
-                  <CloseButton onClick={() => setOpen1(!open1)} />
-                  <dl className="row ps-5">
-                    <dt className="col-12">
-                      <span className="text-danger lead">Porject Name 1</span>
-                    </dt>
-                    <dt className="col-4">CLIENT</dt>
-                    <dd className="col-8">Abc Company</dd>
-                    <dt className="col-4">CATEGORY</dt>
-                    <dd className="col-8">Branding</dd>
-                    <dt className="col-4">DURATION</dt>
-                    <dd className="col-8">2 weeks</dd>
-                  </dl>
-                </Col>
-                <Col sm="6" xl="3" className="p-0">
-                  <Image
-                    src="https://images.unsplash.com/photo-1517048676732-d65bc937f952?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-                    fluid
-                  />
-                </Col>
-                <Col sm="6" xl="3" className="p-0">
-                  <Image
-                    src="https://images.unsplash.com/photo-1552664730-d307ca884978?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-                    fluid
-                  />
-                </Col>
-                <Col sm="6" xl="3" className="p-0">
-                  <Image
-                    src="https://images.unsplash.com/photo-1606857521015-7f9fcf423740?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-                    fluid
-                  />
-                </Col>
-              </Row>
-            </Container>
-          </Collapse>
-
-          <Collapse in={open2}>
-            <Container fluid className="p-0 mt-5">
-              <Row className="p-0" shadow="true">
-                <Col sm="6" xl="3">
-                  <CloseButton onClick={() => setOpen2(!open2)} />
-                  <dl className="row ps-5">
-                    <dt className="col-12">
-                      <span className="text-danger lead">Porject Name 2</span>
-                    </dt>
-                    <dt className="col-4">lorem0</dt>
-                    <dd className="col-8">Klm Company</dd>
-                    <dt className="col-4">CATEGORY</dt>
-                    <dd className="col-8">Branding</dd>
-                    <dt className="col-4">DURATION</dt>
-                    <dd className="col-8">2 weeks</dd>
-                  </dl>
-                </Col>
-                <Col sm="6" xl="3" className="p-0">
-                  <Image
-                    src="https://images.unsplash.com/photo-1517048676732-d65bc937f952?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-                    fluid
-                  />
-                </Col>
-                <Col sm="6" xl="3" className="p-0">
-                  <Image
-                    src="https://images.unsplash.com/photo-1552664730-d307ca884978?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-                    fluid
-                  />
-                </Col>
-                <Col sm="6" xl="3" className="p-0">
-                  <Image
-                    src="https://images.unsplash.com/photo-1606857521015-7f9fcf423740?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-                    fluid
-                  />
-                </Col>
-              </Row>
-            </Container>
-          </Collapse>
-
-          <Collapse in={open3}>
-            <Container fluid className="p-0 mt-5">
-              <Row className="p-0">
-                <Col sm="6" xl="3">
-                  <CloseButton onClick={() => setOpen3(!open3)} />
-                  <dl className="row ps-5">
-                    <dt className="col-12">
-                      <span className="text-danger lead">Porject Name 3</span>
-                    </dt>
-                    <dt className="col-4">CLIENT</dt>
-                    <dd className="col-8">XYZ Company</dd>
-                    <dt className="col-4">CATEGORY</dt>
-                    <dd className="col-8">Branding</dd>
-                    <dt className="col-4">DURATION</dt>
-                    <dd className="col-8">2 weeks</dd>
-                  </dl>
-                </Col>
-                <Col sm="6" xl="3" className="p-0">
-                  <Image
-                    src="https://images.unsplash.com/photo-1517048676732-d65bc937f952?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-                    fluid
-                  />
-                </Col>
-                <Col sm="6" xl="3" className="p-0">
-                  <Image
-                    src="https://images.unsplash.com/photo-1552664730-d307ca884978?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-                    fluid
-                  />
-                </Col>
-                <Col sm="6" xl="3" className="p-0">
-                  <Image
-                    src="https://images.unsplash.com/photo-1606857521015-7f9fcf423740?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-                    fluid
-                  />
-                </Col>
-              </Row>
-            </Container>
-          </Collapse>
-
-          <Collapse in={open4}>
-            <Container fluid className="p-0 mt-5">
-              <Row className="p-0" shadow="true">
-                <Col sm="6" xl="3">
-                  <CloseButton onClick={() => setOpen4(!open4)} />
-                  <dl className="row ps-5">
-                    <dt className="col-12">
-                      <span className="text-danger lead">Porject Name 4</span>
-                    </dt>
-                    <dt className="col-4">CLIENT</dt>
-                    <dd className="col-8">Asd Company</dd>
-                    <dt className="col-4">CATEGORY</dt>
-                    <dd className="col-8">Branding</dd>
-                    <dt className="col-4">DURATION</dt>
-                    <dd className="col-8">2 weeks</dd>
-                  </dl>
-                </Col>
-                <Col sm="6" xl="3" className="p-0">
-                  <Image
-                    src="https://images.unsplash.com/photo-1517048676732-d65bc937f952?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-                    fluid
-                  />
-                </Col>
-                <Col sm="6" xl="3" className="p-0">
-                  <Image
-                    src="https://images.unsplash.com/photo-1552664730-d307ca884978?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-                    fluid
-                  />
-                </Col>
-                <Col sm="6" xl="3" className="p-0">
-                  <Image
-                    src="https://images.unsplash.com/photo-1606857521015-7f9fcf423740?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-                    fluid
-                  />
-                </Col>
-              </Row>
-            </Container>
-          </Collapse> */}
         </Row>
       </Container>
     </section>
