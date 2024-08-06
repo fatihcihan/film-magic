ac2497e85c56badbd82f5ca986a95104ae8fb825
Fatih <fatihcihan666@gmail.com>
2024-08-06 14:01:08 +0300

    Integrated tmdb API for displaying series genres and details in modals

--- src/components/TeamSection/TeamSection.tsx
+++ src/components/TeamSection/TeamSection.tsx
@@ -1,4 +1,5 @@
-import React from "react";
+import axios, { AxiosResponse } from "axios";
+import React, { useEffect, useState } from "react";
 import {
   Button,
   ButtonGroup,
@@ -8,12 +9,61 @@ import {
   Modal,
   Row,
 } from "react-bootstrap";
+import { SerieGenre } from "../../types/SerieGenre";
 
-function TeamSection() {
-  const [modalShow1, setModalShow1] = React.useState(false);
-  const [modalShow2, setModalShow2] = React.useState(false);
-  const [modalShow3, setModalShow3] = React.useState(false);
-  const [modalShow4, setModalShow4] = React.useState(false);
+const TeamSection = () => {
+  const [openGenre, setOpenGenre] = useState<number | null>(null);
+  const [genres, setGenres] = useState<SerieGenre[] | null>([]);
+  const [selectedGenreData, setSelectedGenreData] = useState<any>(null);
+  const buttonColors = ["danger", "warning", "success", "primary"];
+  const accessToken = process.env.REACT_APP_API_ACCESS_TOKEN;
+
+  const fetchSerieGenres = async (): Promise<void> => {
+    try {
+      const response: AxiosResponse<any> = await axios.get(
+        `https://api.themoviedb.org/3/genre/tv/list?language=en`,
+        {
+          headers: {
+            Accept: "application/json",
+            Authorization: `Bearer ${accessToken}`,
+          },
+        }
+      );
+      const genres: SerieGenre[] = response.data.genres;
+      setGenres(genres);
+    } catch (err) {
+      console.log("error");
+    }
+  };
+
+  const fetchSeriesByGenre = async (genreId: number): Promise<void> => {
+    try {
+      const response: AxiosResponse<any> = await axios.get(
+        `https://api.themoviedb.org/3/discover/tv?with_genres=${genreId}&language=en`,
+        {
+          headers: {
+            Accept: "application/json",
+            Authorization: `Bearer ${accessToken}`,
+          },
+        }
+      );
+      setSelectedGenreData(response.data.results[0]);
+      console.log(response.data.results, "data results");
+    } catch (err) {
+      console.log("error fetching genre details");
+    }
+  };
+
+  useEffect(() => {
+    fetchSerieGenres();
+  }, []);
+
+  useEffect(() => {
+    if (openGenre !== null) {
+      fetchSeriesByGenre(openGenre);
+    }
+  }, [openGenre]);
+
   return (
     <div>
       <section id="team" className="generic">
@@ -22,76 +72,37 @@ function TeamSection() {
         </Container>
         <Container fluid>
           <Row>
-            <Col sm="6" lg="3" className="p-0">
-              <div className="d-grid bg-danger py-5">
-                <Button
-                  className="rounded-0"
-                  variant="danger"
-                  onClick={() => setModalShow1(true)}
-                >
-                  <span className="lead"> DESIGNER</span>
-                </Button>
-                <TeamMemberModal1
-                  show={modalShow1}
-                  onHide={() => setModalShow1(false)}
-                />
-              </div>
-            </Col>
-            <Col sm="6" lg="3" className="p-0">
-              <div className="d-grid bg-warning py-5">
-                <Button
-                  className="rounded-0"
-                  variant="warning"
-                  onClick={() => setModalShow2(true)}
-                >
-                  <span className="lead">DEVELOPER</span>
-                </Button>
-                <TeamMemberModal2
-                  show={modalShow2}
-                  onHide={() => setModalShow2(false)}
-                />
-              </div>
-            </Col>
-            <Col sm="6" lg="3" className="p-0">
-              <div className="d-grid bg-success py-5">
-                <Button
-                  className="rounded-0"
-                  variant="success"
-                  onClick={() => setModalShow3(true)}
-                >
-                  <span className="lead">ENGINEER</span>
-                </Button>
-                <TeamMemberModal3
-                  show={modalShow3}
-                  onHide={() => setModalShow3(false)}
-                />
-              </div>
-            </Col>
-            <Col sm="6" lg="3" className="p-0">
-              <div className="d-grid bg-primary py-5">
-                <Button
-                  className="rounded-0"
-                  variant="primary"
-                  onClick={() => setModalShow4(true)}
-                >
-                  <span className="lead">CREATOR</span>
-                </Button>
-                <TeamMemberModal4
-                  show={modalShow4}
-                  onHide={() => setModalShow4(false)}
-                />
-              </div>
-            </Col>
+            {genres?.slice(0, 4).map((genre, index) => (
+              <Col sm="6" lg="3" className="p-0" key={genre.id}>
+                <div className={`d-grid bg-${buttonColors[index]} py-5`}>
+                  <Button
+                    className="rounded-0"
+                    variant={buttonColors[index]}
+                    onClick={() =>
+                      setOpenGenre(openGenre === genre.id ? null : genre.id)
+                    }
+                  >
+                    <span className="lead"> {genre.name}</span>
+                  </Button>
+                  <TeamMemberModal1
+                    show={openGenre === genre.id}
+                    onHide={() => setOpenGenre(null)}
+                    genreData={selectedGenreData}
+                  />
+                </div>
+              </Col>
+            ))}
           </Row>
         </Container>
       </section>
     </div>
   );
-}
+};
 
 export default TeamSection;
 
 function TeamMemberModal1(props: any) {
+  const { genreData } = props;
   return (
     <Modal
       {...props}
@@ -101,196 +112,36 @@ function TeamMemberModal1(props: any) {
     >
       <Modal.Header closeButton>
         <Modal.Title id="contained-modal-title-vcenter">
-          Modal heading
+          {genreData?.name || "Loading..."}
         </Modal.Title>
       </Modal.Header>
       <Modal.Body>
         <Container>
           <Row>
-            <Image
-              className="img-fluid"
-              src="https://images.unsplash.com/photo-1546514714-df0ccc50d7bf?q=80&w=1374&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-            />
-            <h4 className="text-danger lead mt-2">Jon Doe</h4>
-            <p>
-              Cras mattis consectetur purus sit amet fermentum. Cras justo odio,
-              dapibus ac facilisis in, egestas eget quam. Morbi leo risus, porta
-              ac consectetur ac, vestibulum at eros.
-            </p>
-            <ul className="list-unstyled">
-              <li>Lorem ipsum dolor sit amet.</li>
-              <li>Lorem ipsum dolor sit amet.</li>
-              <li>Lorem ipsum dolor sit amet.</li>
-              <ul>
-                <li>Lorem ipsum dolor sit amet.</li>
-                <li>Lorem ipsum dolor sit amet.</li>
-              </ul>
-              <li>Lorem ipsum dolor sit amet.</li>
-            </ul>
+            {genreData ? (
+              <>
+                <div className="text-center">
+                  <Image
+                    className="img-fluid"
+                    src={`https://image.tmdb.org/t/p/w500${genreData.poster_path}`}
+                    style={{
+                      maxWidth: "340px",
+                      maxHeight: "420px",
+                      width: "100%",
+                      height: "auto",
+                    }}
+                  />
+                </div>
+                <h4 className="text-danger lead mt-2 display-5">
+                  {genreData.name}
+                </h4>
+                <p>{genreData.overview}</p>
+              </>
+            ) : (
+              <p>Loading...</p>
+            )}
           </Row>
         </Container>
-
-        <ButtonGroup className="mb-2 btn-group w-100">
-          <Button variant="danger">LINKEDIN</Button>
-          <Button variant="warning">FACEBOOK</Button>
-          <Button variant="success">TWITTER</Button>
-        </ButtonGroup>
-      </Modal.Body>
-      <Modal.Footer>
-        <Button onClick={props.onHide}>Close</Button>
-      </Modal.Footer>
-    </Modal>
-  );
-}
-
-function TeamMemberModal2(props: any) {
-  return (
-    <Modal
-      {...props}
-      size="lg"
-      aria-labelledby="contained-modal-title-vcenter"
-      centered
-    >
-      <Modal.Header closeButton>
-        <Modal.Title id="contained-modal-title-vcenter">
-          Modal heading
-        </Modal.Title>
-      </Modal.Header>
-      <Modal.Body>
-        <Container>
-          <Row>
-            <Image
-              className="img-fluid"
-              src="https://images.unsplash.com/photo-1580894908361-967195033215?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mjh8fGRldmVsb3BlcnxlbnwwfHwwfHx8MA%3D%3D"
-            />
-            <h4 className="text-danger lead mt-2">Jon Doe</h4>
-            <p>
-              Cras mattis consectetur purus sit amet fermentum. Cras justo odio,
-              dapibus ac facilisis in, egestas eget quam. Morbi leo risus, porta
-              ac consectetur ac, vestibulum at eros.
-            </p>
-            <ul className="list-unstyled">
-              <li>Lorem ipsum dolor sit amet.</li>
-              <li>Lorem ipsum dolor sit amet.</li>
-              <li>Lorem ipsum dolor sit amet.</li>
-              <ul>
-                <li>Lorem ipsum dolor sit amet.</li>
-                <li>Lorem ipsum dolor sit amet.</li>
-              </ul>
-              <li>Lorem ipsum dolor sit amet.</li>
-            </ul>
-          </Row>
-        </Container>
-
-        <ButtonGroup className="mb-2 btn-group w-100">
-          <Button variant="danger">LINKEDIN</Button>
-          <Button variant="warning">FACEBOOK</Button>
-          <Button variant="success">TWITTER</Button>
-        </ButtonGroup>
-      </Modal.Body>
-      <Modal.Footer>
-        <Button onClick={props.onHide}>Close</Button>
-      </Modal.Footer>
-    </Modal>
-  );
-}
-
-function TeamMemberModal3(props: any) {
-  return (
-    <Modal
-      {...props}
-      size="lg"
-      aria-labelledby="contained-modal-title-vcenter"
-      centered
-    >
-      <Modal.Header closeButton>
-        <Modal.Title id="contained-modal-title-vcenter">
-          Modal heading
-        </Modal.Title>
-      </Modal.Header>
-      <Modal.Body>
-        <Container>
-          <Row>
-            <Image
-              className="img-fluid"
-              src="https://images.unsplash.com/photo-1528901166007-3784c7dd3653?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-            />
-            <h4 className="text-danger lead mt-2">Jon Doe</h4>
-            <p>
-              Cras mattis consectetur purus sit amet fermentum. Cras justo odio,
-              dapibus ac facilisis in, egestas eget quam. Morbi leo risus, porta
-              ac consectetur ac, vestibulum at eros.
-            </p>
-            <ul className="list-unstyled">
-              <li>Lorem ipsum dolor sit amet.</li>
-              <li>Lorem ipsum dolor sit amet.</li>
-              <li>Lorem ipsum dolor sit amet.</li>
-              <ul>
-                <li>Lorem ipsum dolor sit amet.</li>
-                <li>Lorem ipsum dolor sit amet.</li>
-              </ul>
-              <li>Lorem ipsum dolor sit amet.</li>
-            </ul>
-          </Row>
-        </Container>
-
-        <ButtonGroup className="mb-2 btn-group w-100">
-          <Button variant="danger">LINKEDIN</Button>
-          <Button variant="warning">FACEBOOK</Button>
-          <Button variant="success">TWITTER</Button>
-        </ButtonGroup>
-      </Modal.Body>
-      <Modal.Footer>
-        <Button onClick={props.onHide}>Close</Button>
-      </Modal.Footer>
-    </Modal>
-  );
-}
-
-function TeamMemberModal4(props: any) {
-  return (
-    <Modal
-      {...props}
-      size="lg"
-      aria-labelledby="contained-modal-title-vcenter"
-      centered
-    >
-      <Modal.Header closeButton>
-        <Modal.Title id="contained-modal-title-vcenter">
-          Modal heading
-        </Modal.Title>
-      </Modal.Header>
-      <Modal.Body>
-        <Container>
-          <Row>
-            <Image
-              className="img-fluid"
-              src="https://images.unsplash.com/photo-1521999693742-4717d76f97cc?q=80&w=1469&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-            />
-            <h4 className="text-danger lead mt-2">Jon Doe</h4>
-            <p>
-              Cras mattis consectetur purus sit amet fermentum. Cras justo odio,
-              dapibus ac facilisis in, egestas eget quam. Morbi leo risus, porta
-              ac consectetur ac, vestibulum at eros.
-            </p>
-            <ul className="list-unstyled">
-              <li>Lorem ipsum dolor sit amet.</li>
-              <li>Lorem ipsum dolor sit amet.</li>
-              <li>Lorem ipsum dolor sit amet.</li>
-              <ul>
-                <li>Lorem ipsum dolor sit amet.</li>
-                <li>Lorem ipsum dolor sit amet.</li>
-              </ul>
-              <li>Lorem ipsum dolor sit amet.</li>
-            </ul>
-          </Row>
-        </Container>
-
-        <ButtonGroup className="mb-2 btn-group w-100">
-          <Button variant="danger">LINKEDIN</Button>
-          <Button variant="warning">FACEBOOK</Button>
-          <Button variant="success">TWITTER</Button>
-        </ButtonGroup>
       </Modal.Body>
       <Modal.Footer>
         <Button onClick={props.onHide}>Close</Button>
