cb9a49943b9ebbafd8a68f6ee4bac6dbfc006ff4
Fatih <fatihcihan666@gmail.com>
2024-08-17 01:46:27 +0300

    Background color updated to dark.

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -4,7 +4,7 @@ const Footer: React.FC = (): JSX.Element => {
   const currentYear = new Date().getFullYear();
   return (
     <footer>
-      <Container fluid className="bg-danger text-center py-4 lead text-light">
+      <Container fluid className="bg-dark text-center py-4 lead text-light">
         NO &copy; {currentYear} FILM-MAGIC INC
       </Container>
     </footer>
